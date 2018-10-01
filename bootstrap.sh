61728b262c8c4e96b80d174a206229e77f26f1c7
Ke Liu <ke.frank.liu@gmail.com>
2018-10-01 20:24:55 +1000

    Build googltest from the remote github dump

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,11 @@
 
 BUILD_DIR="./build/"
 mkdir -p ${BUILD_DIR}
+
+EXTRA="
+-DCMAKE_BUILD_TYPE=Debug
+"
+
+pushd ${BUILD_DIR}
+cmake ../ --debug-output --trace-expand ${EXTRA}
+popd
--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -6,8 +6,9 @@ ExternalProject_Add(gtest
 	PREFIX 		"googletest"
 	GIT_REPOSITORY 	"https://github.com/google/googletest.git"
 	GIT_TAG 	"2fe3bd994b3189899d93f1d5a881e725e046fdc2" #release-1.8.1
-	BINARY_DIR	${CMAKE_BINARY_DIR}/src/googletest
-	SOURCE_DIR	${CMAKE_BINARY_DIR}/src/googletest
+	BINARY_DIR	${CMAKE_BINARY_DIR}/gtest
+	SOURCE_DIR	${CMAKE_BINARY_DIR}/third_party/gtest
+        INSTALL_COMMAND cmake -E echo "Skipping install gtest."
 	LOG_BUILD ON
 	)	
 
@@ -19,11 +20,11 @@ ExternalProject_Get_Property(gtest SOURCE_DIR BINARY_DIR)
 set_target_properties(libgtest PROPERTIES
     "IMPORTED_LOCATION" "${BINARY_DIR}/libgtest.a"
     "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-    "INTERFACE_INCLUDE_DIRECTORIES" "${SOURCE_DIR}/include"
+    "INTERFACE_INCLUDE_DIRECTORIES" "${SOURCE_DIR}/googletest/include"
     )
 
 set_target_properties(libgtest_main PROPERTIES
     "IMPORTED_LOCATION" "${BINARY_DIR}/libgtest_main.a"
     "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-    "INTERFACE_INCLUDE_DIRECTORIES" "${SOURCE_DIR}/include"
+    "INTERFACE_INCLUDE_DIRECTORIES" "${SOURCE_DIR}/googletest/include"
     )
